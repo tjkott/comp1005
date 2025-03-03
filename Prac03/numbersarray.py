2a30f70bc7e443e2c63819eab7e4fd3176c9f70b
Theja <thejana.kottawatta@gmail.com>
2025-03-03 15:00:24 +0800

    v

--- Prac03/numbersarray.py
+++ Prac03/numbersarray.py
@@ -1,11 +1,28 @@
 #
-# numbersarray.py
+# numbersarray.py: Read ten numbers give sum, min, max and mean. 
 #
-print("Enter ten number...")
-total = 0
+import numpy as np
+import matplotlib.pyplot as plt
 
-for i in  range(10):
+numarray = np.zeros(5) # create an empty 10 element array
+
+print("Enter ten number...")
+
+for i in  range(len(numarray)):
     print('Enter a number (', i, ')...')
-    number = int(input())
-    total = total + number
-print('Total is ', total)
+    numarray[i] = int(input())
+
+xaxis = ['Total', 'Minimum', 'Maximum', 'Mean']
+yaxis = []
+print('Total is ', numarray.sum())
+yaxis.append(numarray.sum())
+print('Minimum is ', numarray.min())
+yaxis.append(numarray.min())
+print('Maximum is ', numarray.max())
+yaxis.append(numarray.max())
+print('Mean is ', numarray.mean())
+yaxis.append(numarray.mean())
+
+plt.title('Plot of the Numbers')
+plt.plot(xaxis, yaxis, marker = '^', linestyle = '', color = 'r')
+plt.show()
