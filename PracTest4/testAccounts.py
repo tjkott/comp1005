4a1f5fee530db201ee1b3cd64a5663aeb9ec609b
Theja <thejana.kottawatta@gmail.com>
2025-05-02 23:05:52 +0800

    c

--- PracTest4/testAccounts.py
+++ PracTest4/testAccounts.py
@@ -3,19 +3,24 @@
 
   testAccounts.py - program to test functions of accounts.py
   
-  Student Name   :
-  Student Number :
-  Date/prac time :
+  Student Name   : Thejana Kottawatta Hewage
+  Student Number : 22307822
+  Date/prac time : 
 '''
 from accounts import BankAccount, Portfolio
 
 print('\n<--- Bank Accounts Portfolio --->\n')
 myAccounts = Portfolio()
+# 1. Create "Castle" account
+myAccounts.addAccount("Castle", "999999-1", 1000)
+# 2. Create Shrubbery account. 
+myAccounts.addAccount("Shrubbery", "999999-2", 100)
+# 4. Deposit $100 into the Castle account. 
+myAccounts.deposit("Castle", 100)
+# 5. Withdraw $10 from the Shrubbery account. 
+myAccounts.withdraw("Shrubbery", 10)
+# 6. Withdraw $1000 from the Shrubbery account. 
+myAccounts.withdraw("Shrubbery", 1000)
 
-# add code for tasks here
-
-#e.g.
-#myAccounts.addAccount("Everyday", "1111-007", 1000)
-#myAccounts.deposit("Everyday",200)
-
+# 7. Print the balances of the account. 
 myAccounts.balances()
